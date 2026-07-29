53f34a76a477038c03dec16eca2a0c409210c133
cbunny1990 <cmncc@treinador.pt>
2026-07-29 22:54:29 +0100

    chore: bump cache version para forçar atualização do service worker no telemóvel

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "calisthenics-v5";
+const CACHE = "calisthenics-v6";
 const ASSETS = [
   "./", "./index.html", "./css/styles.css",
   "./js/db.js", "./js/gerador.js", "./js/timer.js", "./js/treino.js", "./js/app.js",
